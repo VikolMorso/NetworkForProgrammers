d5047373b554e4bce86c54d1f57f8a1946e6546e
Vikol Morso <vika.morozova.sokolova@yandex.ru>
2020-12-17 15:40:37 +0300

    Updates

---
+++ precache-manifest.1ac5aa468ce3ac1214c68e0abd88fec1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05948c64702d1f71d7dbab5d8b61e213",
+    "url": "/NetworkForProgrammers/index.html"
+  },
+  {
+    "revision": "d1717b9f02d7794125da",
+    "url": "/NetworkForProgrammers/static/css/3.2b283795.chunk.css"
+  },
+  {
+    "revision": "7e6df4c860b1c3ffb817",
+    "url": "/NetworkForProgrammers/static/css/main.4e7a02be.chunk.css"
+  },
+  {
+    "revision": "38704e25479c402afc50",
+    "url": "/NetworkForProgrammers/static/js/2.2e0cbd12.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/NetworkForProgrammers/static/js/2.2e0cbd12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1717b9f02d7794125da",
+    "url": "/NetworkForProgrammers/static/js/3.419be974.chunk.js"
+  },
+  {
+    "revision": "a124741a98cf68258f43",
+    "url": "/NetworkForProgrammers/static/js/4.d0d42042.chunk.js"
+  },
+  {
+    "revision": "7e6df4c860b1c3ffb817",
+    "url": "/NetworkForProgrammers/static/js/main.f9f73437.chunk.js"
+  },
+  {
+    "revision": "ee3faf87f63edd63a0a5",
+    "url": "/NetworkForProgrammers/static/js/runtime-main.5c2480b5.js"
+  },
+  {
+    "revision": "e03304b95e078d1df0a4ac01a2fedf3b",
+    "url": "/NetworkForProgrammers/static/media/img1.e03304b9.png"
+  },
+  {
+    "revision": "e32b98735618c859ce4d6479d7660496",
+    "url": "/NetworkForProgrammers/static/media/preloader.e32b9873.gif"
+  }
+]);
